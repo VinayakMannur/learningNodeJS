348438c7c01b2c5723dd7c8c9b16dee50cbe0137
VinayakMannur <Vinayakmannur20@gmail.com>
2023-09-01 18:18:19 +0530

    running on port 4000 and logging name

--- Hello.js
+++ Hello.js
@@ -1,1 +1,38 @@
-console.log('Hello world');
+// console.log('Hello world');
+
+// const fs = require('fs');
+// fs.writeFileSync('hello.txt','hello from Node JS');
+
+
+
+const printc = () =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('c')
+            resolve()
+        },3000)
+    });
+    
+}
+
+const printd = () =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('d')
+            resolve()
+        },0)
+    });
+    
+}
+
+async function print(){
+    console.log('a');
+    console.log('b')
+    await printc()
+    await printd()
+    console.log('e')
+}
+
+print()
+
+
