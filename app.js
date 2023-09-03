ecb5ea921584013771e732640589a67098b1d0ac
VinayakMannur <Vinayakmannur20@gmail.com>
2023-09-03 17:06:10 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middle ware');
-    next();
+app.use(bodyParser.urlencoded({extended:true}));
+
+// app.use('/',(req,res,next)=>{
+//     console.log('this always runs');
+//     next();
+// })
+
+app.use('/add-product',(req,res,next)=>{
+    // console.log('in the amother ware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="title"><button type="submit">Add product</button></form>');
 })
 
-app.use((req,res,next)=>{
-    console.log('in the amother ware');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    // console.log('in the amother ware');
     res.send('<h1> hello to node js </h1>');
     // res.send( { key1: 2 });
 })
